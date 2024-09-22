8462390d985254288f0587eae0a7da1d15c0dcbb
Tomáš Mîčka <tomas.micka@logmanager.com>
2024-09-22 13:39:20 +0200

    new migrations

--- db/migrations/migrate.py
+++ db/migrations/migrate.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
 	client.drop_database("inpv")
 
 	print('[ ] - Migrating Django Course')
-	with open('/usr/src/db/Django.json') as f:
+	with open('/usr/src/db/Django a Client-Server aplikace.json') as f:
 		file_data = json.load(f)
 		course_data = file_data['course']
 		database.courses.insert_one(course_data)
